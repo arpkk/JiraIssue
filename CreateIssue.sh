0b183b1631978fbc020f9811431ae2139416008d
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2022-12-07 11:31:02 -0300

    Update CreateIssue.sh

--- CreateIssue.sh
+++ CreateIssue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "\"$TEXT\""
+echo \" "$TEXT" \"
 
 curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/issue?textoAEnviar=Test' \
 --header 'Authorization: Basic Z2luYS5vemltaXNhQHRzb2Z0Z2xvYmFsLmNvbTpQclRkRkdiNmxvMEtLQXk5Rzd0cTI2NkM=' \
@@ -8,7 +8,7 @@ curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/is
 --header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
 --data-raw '{
     "fields": {
-        "summary": "texto desde jenkins",
+        "summary": \" "$TEXT" \",
         "issuetype": {
             "id": "10002"
         },
@@ -23,7 +23,7 @@ curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/is
                     "type": "paragraph",
                     "content": [
                         {
-                            "text": "texto desde jenkins",
+                            "text": \" "$TEXT" \",
                             "type": "text"
                         }
                     ]
