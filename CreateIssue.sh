41f6d400f92c269041aaa63343d707e5693d027d
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2022-12-07 00:42:58 -0300

    Create CreateIssue.sh

---
+++ CreateIssue.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pc $1
+
+curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/issue?textoAEnviar=Test' \
+--header 'Authorization: Basic Z2luYS5vemltaXNhQHRzb2Z0Z2xvYmFsLmNvbTpQclRkRkdiNmxvMEtLQXk5Rzd0cTI2NkM=' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
+--data-raw '{
+    "fields": {
+        "summary": "$1",
+        "issuetype": {
+            "id": "10002"
+        },
+        "project": {
+            "key": "IR"
+        },
+        "description": {
+            "type": "doc",
+            "version": 1,
+            "content": [
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "text": "$1",
+                            "type": "text"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}' > log.txt
